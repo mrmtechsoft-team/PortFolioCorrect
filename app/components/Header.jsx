974a52c61ef4f01af2a70c3cb7403a15141e3bc2
mrmtechsoft-team <mrmtechsoft@gmail.com>
2025-11-07 02:43:05 +0530

    Update logo image class with margin-top
    Add margin-top to logo image in Header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = (isDarkMode, setIsDakMode) => {
         transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
       >
 
-        <Image src={assets.logoRem} alt='' className=' rounded-full w-60' />
+        <Image src={assets.logoRem} alt='' className=' rounded-full w-60 mt-30' />
       </motion.div>
 
       <motion.div
